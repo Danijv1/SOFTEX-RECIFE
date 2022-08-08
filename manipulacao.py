c0e20a012c072641d6f488f93d414ce4637a2f21
Danijv1 <108374810+Danijv1@users.noreply.github.com>
2022-08-07 21:35:29 -0300

    Add files via upload

---
+++ manipulacao.py
@@ -0,0 +1,8 @@
+name = str(input('Qual o seu nome? '))
+print (f'Seu nome {name} tem', len(name) ,'letras')
+
+name = str(input('Qual o seu nome? '))
+print (f'Seu nome {name} ao contrário é', name[::-1])
+
+name = str(input('Qual o seu nome? '))
+print (f'A 4a letra de {name} é', name[3])
